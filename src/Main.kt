8f4cb67848a4d7f04204748957ebaab53da71ff3
Andres Orejuela <andresforejuelab@gmail.com>
2019-09-08 16:47:42 -0500

    Add functions makeRecipe() and viewRecipe().

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,21 @@
 import kotlin.system.exitProcess
 
-fun main(args: Array<String>) {
 
-    var recipes: MutableList<Any> = mutableListOf<Any>()
-
-    do {
-        println(":: Bienvenido a Recipe Maker ::")
-
-        println("""
+fun viewRecipe() {
+    println(
+        """
+            :: Bienvenido a Recipe Maker ::
+        
             Selecciona la opción deseada
             1. Hacer una receta
             2. Ver mis recetas
             3. Salir
         """.trimIndent()
-        )
+    )
+}
 
-        var response: Int? = readLine()?.toInt()
-        val ingredientsList: List<String> =
-            listOf("Agua", "Leche", "Carne", "Verduras", "Frutas", "Cereal", "Huevos", "Aceite")
-        val ingredientsString: String = """
+fun makeRecipe() {
+    val ingredientsString: String = """
             | 1.Agua
             | 2.Leche
             | 3.Carne
@@ -29,12 +26,25 @@ fun main(args: Array<String>) {
             | 8.Aceite
             | 0.Terminar
         """.trimIndent()
+    println(ingredientsString)
+}
+
+fun main(args: Array<String>) {
+
+    var recipes: MutableList<Any> = mutableListOf<Any>()
+
+    do {
+        viewRecipe()
+
+        var response: Int? = readLine()?.toInt()
+        val ingredientsList: List<String> =
+            listOf("Agua", "Leche", "Carne", "Verduras", "Frutas", "Cereal", "Huevos", "Aceite")
 
         when (response) {
             1 -> {
                 var recipe: MutableList<String> = mutableListOf<String>()
                 do {
-                    println(ingredientsString)
+                    makeRecipe()
                     var item: Int = readLine()?.toInt() ?: 0
                     if (item != 0) {
                         recipe.add(ingredientsList[item.minus(1)])
